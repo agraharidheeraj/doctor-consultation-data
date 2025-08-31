51d4b029352cadae50a8d3f422061fd66e09ebbc
desistack7 <desistack7@gmail..com>
2025-08-31 20:27:21 +0530

    added star sbg

--- constant.ts
+++ constant.ts
@@ -13,6 +13,11 @@ import { Facebook, Instagram, Linkedin, Twitter ,Phone, Mail, MapPin, Video, Clo
           //       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
           //     </svg>
 
+          //star
+                // <svg key={i} className="w-6 h-6 fill-current" viewBox="0 0 24 24">
+                //   <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"/>
+                // </svg>
+
 export const healthcareCategories = [
   {
     id: 'primary-care',
