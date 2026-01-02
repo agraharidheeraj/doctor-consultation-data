d6aa854b9a54ca2a3357db9f7a98615efbb2ff32
Dheeraj agrahari <93854270+agraharidheeraj@users.noreply.github.com>
2026-01-02 15:45:01 +0530

    Add COLORS constant for chart color schema

--- constant.ts
+++ constant.ts
@@ -298,6 +298,7 @@ export const startOfDay = (date: Date): Date => {
       },
     };
 
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#8884D8'];
 
 
   //  enum: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'] 
